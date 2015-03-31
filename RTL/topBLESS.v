57a4960991b5564bbd6b31bd222bd2bdf5cb11d1
anderson1008 <anderson.xxy@gmail.com>
2015-03-31 14:19:16 -0500

    MultiNoC_RTL_v1.2
    Adjust Pipeline: RC&PN + PA&XT
    Also, local injected and bypass flit skip the first stage.

--- RTL/topBLESS.v
+++ RTL/topBLESS.v
@@ -56,23 +56,17 @@ always @ (posedge clk or negedge reset) begin
    end
 end
 
-
 wire [`WIDTH_PV-1:0] prodVector [0:`NUM_PORT-1];
 routeComp routeCompWest (r_dinW[`WIDTH_INTERNAL-1], r_dinW[`POS_X_DST], r_dinW[`POS_Y_DST], prodVector[0]);
 routeComp routeCompEast (r_dinE[`WIDTH_INTERNAL-1], r_dinE[`POS_X_DST], r_dinE[`POS_Y_DST], prodVector[1]);
 routeComp routeCompSouth (r_dinS[`WIDTH_INTERNAL-1], r_dinS[`POS_X_DST], r_dinS[`POS_Y_DST], prodVector[2]);
 routeComp routeCompNorth (r_dinN[`WIDTH_INTERNAL-1], r_dinN[`POS_X_DST], r_dinN[`POS_Y_DST], prodVector[3]);
-routeComp routeCompLocal (r_dinLocal[`WIDTH_INTERNAL-1], r_dinLocal[`POS_X_DST], r_dinLocal[`POS_Y_DST], prodVector[4]);
-routeComp routeCompBypass (r_dinBypass[`WIDTH_INTERNAL-1], r_dinBypass[`POS_X_DST], r_dinBypass[`POS_Y_DST], prodVector[5]);
 
 wire [`WIDTH_INTERNAL_PV-1:0] PNin  [0:`NUM_PORT-3];
-wire [`WIDTH_INTERNAL_PV-1:0] LocalWithPV, BypassWithPV;
 assign PNin[0] = {prodVector[0],r_dinW};
 assign PNin[1] = {prodVector[1],r_dinE};
 assign PNin[2] = {prodVector[2],r_dinS};
 assign PNin[3] = {prodVector[3],r_dinN};
-assign LocalWithPV = {prodVector[4],r_dinLocal};      
-assign BypassWithPV = {prodVector[5],r_dinBypass}; 
 
 wire [`WIDTH_INTERNAL_PV-1:0] PNout0, PNout1, PNout2, PNout3;
 permutationNetwork permutationNetwork (
@@ -86,31 +80,40 @@ PNout2,
 PNout3
 );
 
-reg [`WIDTH_INTERNAL_PV-1:0] pipeline_reg1  [0:`NUM_PORT-1];
+reg [`WIDTH_INTERNAL_PV-1:0] pipeline_reg1  [0:`NUM_PORT-3];
 always @ (posedge clk or negedge reset) begin
    if (~reset) begin
       pipeline_reg1[0] <= 0;
       pipeline_reg1[1] <= 0;
       pipeline_reg1[2] <= 0;
       pipeline_reg1[3] <= 0;
-      pipeline_reg1[4] <= 0;
-      pipeline_reg1[5] <= 0;
    end
    else begin
       pipeline_reg1[0] <= PNout0;
       pipeline_reg1[1] <= PNout1;
       pipeline_reg1[2] <= PNout2;
-      pipeline_reg1[3] <= PNout3;
-      pipeline_reg1[4] <= LocalWithPV;
-      pipeline_reg1[5] <= BypassWithPV;     
+      pipeline_reg1[3] <= PNout3;   
    end
 end
 
+wire [`WIDTH_INTERNAL_PV-1:0] LocalWithPV, BypassWithPV;
+
+routeComp routeCompLocal (r_dinLocal[`WIDTH_INTERNAL-1], r_dinLocal[`POS_X_DST], r_dinLocal[`POS_Y_DST], prodVector[4]);
+routeComp routeCompBypass (r_dinBypass[`WIDTH_INTERNAL-1], r_dinBypass[`POS_X_DST], r_dinBypass[`POS_Y_DST], prodVector[5]);
+assign LocalWithPV = {prodVector[4],r_dinLocal};      
+assign BypassWithPV = {prodVector[5],r_dinBypass}; 
+
 wire [`NUM_PORT*`WIDTH_PV-1:0] reqVector;
 wire [`NUM_PORT*`NUM_PORT-1:0] allocVector;
-assign reqVector = {pipeline_reg1[4][`POS_PV],pipeline_reg1[3][`POS_PV],pipeline_reg1[2][`POS_PV],pipeline_reg1[1][`POS_PV],pipeline_reg1[0][`POS_PV],pipeline_reg1[5][`POS_PV]};
+assign reqVector = {LocalWithPV[`POS_PV],BypassWithPV[`POS_PV],pipeline_reg1[3][`POS_PV],pipeline_reg1[2][`POS_PV],pipeline_reg1[1][`POS_PV],pipeline_reg1[0][`POS_PV]};
 portAllocWrapper portAllocWrapper (reqVector, allocVector);
 
+// Second Stage: PA + XT;
+wire [`WIDTH_XBAR-1:0] XbarOutW, XbarOutE, XbarOutS, XbarOutN, XbarOutLocal, XbarOutBypass;
+xbar6Ports xbar6Ports (allocVector, pipeline_reg1[0][`WIDTH_XBAR-1:0], pipeline_reg1[1][`WIDTH_XBAR-1:0], pipeline_reg1[2][`WIDTH_XBAR-1:0], pipeline_reg1[3][`WIDTH_XBAR-1:0], LocalWithPV[`WIDTH_XBAR-1:0], BypassWithPV[`WIDTH_XBAR-1:0], XbarOutW, XbarOutE, XbarOutS, XbarOutN, XbarOutLocal, XbarOutBypass);
+
+// Second Stage: PA ; Third Stage: XT
+/*
 // Strip off PV and valid fields.
 // Flit Format pass through Xbar [PktId, FlitId, Time, Xdst, Ydst, payload];
 reg [`WIDTH_XBAR-1:0] pipeline_reg2  [0:`NUM_PORT-1];
@@ -127,12 +130,12 @@ always @ (posedge clk or negedge reset) begin
       pipeline_reg2_allocVector <= 0;
    end
    else begin
-      pipeline_reg2[0] <= PNout0[`WIDTH_XBAR-1:0];
-      pipeline_reg2[1] <= PNout1[`WIDTH_XBAR-1:0];
-      pipeline_reg2[2] <= PNout2[`WIDTH_XBAR-1:0];
-      pipeline_reg2[3] <= PNout3[`WIDTH_XBAR-1:0];
-      pipeline_reg2[4] <= pipeline_reg1[4][`WIDTH_XBAR-1:0];
-      pipeline_reg2[5] <= pipeline_reg1[5][`WIDTH_XBAR-1:0];
+      pipeline_reg2[0] <= pipeline_reg1[0][`WIDTH_XBAR-1:0];
+      pipeline_reg2[1] <= pipeline_reg1[1][`WIDTH_XBAR-1:0];
+      pipeline_reg2[2] <= pipeline_reg1[2][`WIDTH_XBAR-1:0];
+      pipeline_reg2[3] <= pipeline_reg1[3][`WIDTH_XBAR-1:0];
+      pipeline_reg2[4] <= LocalWithPV[`WIDTH_XBAR-1:0];
+      pipeline_reg2[5] <= BypassWithPV[`WIDTH_XBAR-1:0];
       pipeline_reg2_allocVector <= allocVector;      
    end
 end
@@ -140,6 +143,7 @@ end
 wire [`WIDTH_XBAR-1:0] XbarOutW, XbarOutE, XbarOutS, XbarOutN, XbarOutLocal, XbarOutBypass;
 
 xbar6Ports xbar6Ports (pipeline_reg2_allocVector, pipeline_reg2[0], pipeline_reg2[1], pipeline_reg2[2], pipeline_reg2[3], pipeline_reg2[4], pipeline_reg2[5], XbarOutW, XbarOutE, XbarOutS, XbarOutN, XbarOutLocal, XbarOutBypass);
+*/
 
 reg  [`WIDTH_PORT-1:0] r_doutW, r_doutE, r_doutS, r_doutN;
 reg [`WIDTH_PORT-1:0] r_doutLocal, r_doutBypass;
